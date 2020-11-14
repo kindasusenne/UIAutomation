e3d6ea2d6bc5c03c3e3c8a5cc93cdf98b2cc0118
kindasusenne <872266030@qq.com>
2020-11-14 20:46:48 +0800

    支持多浏览器

--- BasePage/Base.py
+++ BasePage/Base.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from time import sleep
+import os
 
 class Base():
     def setup(self):
-        self.driver=webdriver.Chrome()
+        browser = os.getenv("browser")
+        if browser == 'firefox':
+            self.driver=webdriver.firefox()
+        elif browser == 'headless':
+            self.driver = webdriver.PhantomJS()
+        else:
+            self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.driver.maximize_window()
 
